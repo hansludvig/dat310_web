2ca93157a49bd184afe6bdb95580744daa6a0f39
hansludvig <hans.ludvig.kleivdal@outlook.com>
2017-02-20 13:51:26 +0100

    done with exercise6b, CP

---
+++ exercises/js/basics/exercise6b.js
@@ -0,0 +1,21 @@
+
+
+BankAccount.prototype.printTable = function(){
+    document.write("<table><thead><tr>"
+        + "<th>Date</th>"
+        + "<th>Transaction</th>"
+        + "<th>Amount</th>"
+        + "<th>Old balance</th>"
+        + "<th>New balance"
+        + "</tr></thead>");
+    
+    for (var i = 0; i < this.log.length; i++){
+        document.write("<tr class=\"success" + this.log[i].success + "\"><td>"
+            + this.log[i].date + "</td>"
+            + "<td>" + this.log[i].transaction + "</td>"
+            + "<td>" + this.log[i].amount + "</td>"
+            + "<td>" + this.log[i].oldBalance + "</td>"
+            + "<td>" + this.log[i].newBalance + "</td></tr>");
+    }
+    document.write("</table>")
+}

01381ac3b91a3054daebfe544a74b8f6d1aa537b
hansludvig <hans.ludvig.kleivdal@outlook.com>
2017-02-21 13:55:39 +0100

    added assign files

---
+++ assignments/5b/index.js
@@ -0,0 +1,40 @@
+/**
+ * Exercise #8: Card board
+ */
+
+$(document).ready(function() {
+
+    /* dealing cards */
+    $("#start").click(function(e) {
+
+       // e.preventDefault();  // preventing the form from submission
+
+        $("#cardboard").empty();
+
+        var sizeCols = 4;
+        var sizeRows = 4;
+
+        var card;
+        for (var row = 0; row < sizeRows; row++) {
+            for (var col = 0; col < sizeCols; col++) {
+                card = $("<div></div>").addClass("card");
+                if (col == 0) {
+                    card.addClass("clearleft");
+                }
+                $("#cardboard").append(card);
+            }
+        }
+
+        var cardWidth = card.outerWidth(true);
+        $("#cardboard").width(sizeCols * cardWidth);
+
+        // alternatively, we can return false to prevent the form from submitting
+        //return false;
+    });
+
+    /* clicking on a card */
+    $("#cardboard").on("click", ".card", function () {
+        $(this).fadeTo(1000, 0);
+    });
+
+});

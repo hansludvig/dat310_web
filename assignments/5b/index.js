93221e3733103614614586c42d75ce8effaa448b
hansludvig <hans.ludvig.kleivdal@outlook.com>
2017-02-22 20:38:39 +0100

    need to remove flip function

--- assignments/5b/index.js
+++ assignments/5b/index.js
@@ -5,8 +5,13 @@
 $(document).ready(function() {
 
     /* assign randome possition for images */
-    var arr = [0,1,2,3,4,5,6,7];
+    var arr = [0,1,2,3,4,5,6,7,0,1,2,3,4,5,6,7];
+    var imagArr = [];
     
+    for (var i = 0; i < arr.length; i++){
+        var img = 'images/fruit_' + i + '.jpg';
+        imagArr.push(img);
+    }
 
     /* dealing cards */
     $("#start").click(function(e) {
@@ -17,11 +22,26 @@ $(document).ready(function() {
 
         var sizeCols = 4;
         var sizeRows = 4;
+        var j = 0;
 
+        var front;
+        var back;
+        var back2;
         var card;
         for (var row = 0; row < sizeRows; row++) {
             for (var col = 0; col < sizeCols; col++) {
-                card = $("<div></div>").addClass("card");
+                card = $("<div></div>").addClass("flip");
+                back = $("<div></div>").addClass("back");
+                back2 = $("<img src=\"images/fruit_" + j + ".jpg\" alt=" + j + "\" />");
+                back.prepend(back2);
+                front = $("<div></div>").addClass("front");
+                card.prepend(front).prepend(back);
+                
+                if (j == 7){
+                    j = 0;
+                }else{
+                    j++;
+                }
                 if (col == 0) {
                     card.addClass("clearleft");
                 }
@@ -36,9 +56,16 @@ $(document).ready(function() {
         //return false;
     });
 
-    /* clicking on a card */
-    $("#cardboard").on("click", ".card", function () {
-        $(this).fadeTo(1000, 0);
+    /* clicking on a card 
+    $("#cardboard").on("click", ".flip", function () {
+        $(this).flip();
+    });*/
+
+    $(function(){
+        $(".flip").flip({
+            axis: 'y',
+            trigger: 'click'
+        });
     });
 
 });

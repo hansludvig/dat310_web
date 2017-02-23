277f696ae4521a8fb8609212c68ed291c97f71af
hansludvig <hans.ludvig.kleivdal@outlook.com>
2017-02-23 14:52:49 +0100

    need to fix double click

--- assignments/5b/images/test/test1.js
+++ assignments/5b/images/test/test1.js
@@ -1,13 +1,48 @@
+ 
+ $(document).ready(function(){
+    var arr = [0,1,2,3,4,5,6,7,0,1,2,3,4,5,6,7];
+    var imagArr = [];
+    
+    for (var i = 0; i < arr.length; i++){
+        var img = 'images/fruit_' + i + '.jpg';
+        imagArr.push(img);
+    }
 
-$(document).ready(function (){
+    $("#start").click(function(e) {
 
-    var front = $("<div></div>").addClass("front");
-    var back = $("<div></div>").addClass("back");
-    $("#card").prepend(front).prepend(back);
 
-});
+        var sizeCols = 4;
+        var sizeRows = 4;
+        var j = 0;
 
-$("#card").flip({
-    axis: x,
-    trigger: 'click'
-});
+        var front;
+        var back;
+        var back2;
+        var card;
+        for (var row = 0; row < sizeRows; row++) {
+            for (var col = 0; col < sizeCols; col++) {
+                card = $("<div></div>").addClass("card");
+                back = $("<div></div>").addClass("front");
+                back2 = $("<img src=\"../fruit_" + j + ".jpg\" alt=" + j + "\" />");
+                back.prepend(back2);
+                front = $("<div></div>").addClass("back");
+                card.prepend(front).prepend(back);
+                
+                if (j == 7){
+                    j = 0;
+                }else{
+                    j++;
+                }
+                
+                $("body").append(card);
+            }
+        }
+      
+
+    });
+ 
+   $("#cardboard").on("click", ".card", function(){
+        $(this).flip();
+    });
+ });
+   

4b19a6df64e31f3c10dc9e42c69b79e5e5df64f7
hansludvig <hans.ludvig.kleivdal@outlook.com>
2017-03-07 21:37:08 +0100

    gradebook_v2 done, all functions done as spesified in the assign

--- assignments/6b/gradebook_v2.py
+++ assignments/6b/gradebook_v2.py
@@ -15,6 +15,7 @@ class Gradebook(object):
         self.__students = {}  # dict with student_no as key and name as value
         self.__courses = {}
         self.__grades = {}
+        self.__semester_and_course = {}
 
     def insertion_sort(self, a):
 
@@ -63,16 +64,22 @@ class Gradebook(object):
         with open("grades.tsv", "r") as h:
             for line in h:
                 student_no, course_code, semester, grade = line.strip().split("\t")
-                result_list = [course_code, semester, grade]
+                current_result = {semester: {course_code: grade}}
                 if student_no in self.__grades:
-                    self.__grades[student_no].append(result_list)
+                    if semester in self.__grades[student_no]:
+                        if course_code in self.__grades[student_no][semester]:
+                            pass  # if the course is allready added, do nothing.
+                        else:
+                            self.__grades[student_no][semester][course_code] = grade
+                    else:
+                        self.__grades[student_no][semester] = {course_code: grade}
                 else:
-                    self.__grades[student_no] = [result_list]
+                    self.__grades[student_no] = current_result
 
     def __generate_student_files(self):
         print("Generating student file ...")
         for s, n in self.__students.items():
-            sPath = "output/students/{}".format(s)
+            sPath = "output/students/{}.html".format(s)
             with open(sPath, "w") as f:
                 f.write(HTML_FRAME_TOP.replace("{title}", "Grades " + str(s)).replace("{css_path}", "../"))
                 f.write("<h1>Student</h1><table>")
@@ -80,25 +87,83 @@ class Gradebook(object):
                 f.write("<tr><td><strong>Name:</td><td>{}</td></tr>".format(n))
                 f.write("</table><br />\n<table>\n<thead>\n<tr><th>Course code</th><th>Name</th><th>Grade</th></tr>\n"
                         "</thead>\n<tbody>\n")
-                list_of_results = self.insertion_sort(self.__grades[s])  # Sort semesters
-                tmp = '0'
-                for i in list_of_results:
 
-                    if tmp != i[1]:
-                        f.write("<tr><td colspan=\"3\"><em>Semester {}</em></td></tr>".format(i[1]))
-                        f.write("<tr><td>{}</td><td>{}</td><td> B</td></tr>".format(i[0], self.__courses[i[0]]))
-                    else:
-                        f.write("<tr><td>{}</td><td>{}</td><td> B</td></tr>".format(i[0], self.__courses[i[0]]))
-                    tmp = i[1]
+                for semester, subject in self.__grades[s].items():
+                    tmp = 1
+                    for sub, g in subject.items():
+
+                        if len(subject) > 1:
+                            if tmp == 1:
+                                f.write("<tr><td colspan=\"3\"><em>Semester {}</em></td></tr>".format(semester))
+                                tmp = 0
+                            f.write("<tr><td>{}</td><td>{}</td><td>{}</td></tr>".format(sub, self.__courses[sub], g))
+                        else:
+                            f.write("<tr><td colspan=\"3\"><em>Semester {}</em></td></tr>".format(semester))
+                            f.write("<tr><td>{}</td><td>{}</td><td>{}</td></tr>".format(sub, self.__courses[sub], g))
+
                 f.write("</tbody>\n</table>")
                 f.write(HTML_FRAME_BOTTOM)
 
     def __generate_course_files(self):
-       pass
+        print("Generating course files ...")
+        for code, name in self.__courses.items():
+            sPath = "output/courses/{}.html".format(code)
+            with open(sPath, "w") as f:
+                f.write(HTML_FRAME_TOP.replace("{title}", "Course " + code).replace("{css_path}", "../"))
+                f.write("<h1>Course {}</h1><h2>{}</h2><table>".format(code, name))
+                f.write("<thead>\n<tr><th>Student no</th><th>Grade</th></tr>\n</thead>\n<tbody>\n")
+                store_grade = {}
+                for stud, sem in self.__grades.items():
+                    for c, g in sem.items():
+                        for course, grade in g.items():
+                            if course == code:
+                                f.write("<tr><td>{}</td><td>{}</td></tr>".format(stud, grade))
+                                if (store_grade == {}) or ((grade in store_grade) == False):
+                                    store_grade[grade] = 1
+                                else:
+                                    count = store_grade[grade] + 1
+                                    store_grade[grade] = count
+
+                f.write("</tbody>\n</table>\n<h3>Summary</h3><table>\n<thead>\n<tr><th>Grade</th><th>Count</th></tr>"
+                        "\n</thead>\n<tbody>")
+                for g, c in sorted(store_grade.items()):
+                    f.write("<tr><td>{}</td><td>{}</td></tr>".format(g, c))
+                f.write("</tbody>\n</table>\n")
+                f.write(HTML_FRAME_BOTTOM)
 
     def __generate_semester_files(self):
-        """Generates HTML files for semesters."""
-        pass
+        print("Generating semester files ...")
+        for stud, sem in self.__grades.items():
+            for nr, course in sem.items():
+                for c, g in course.items():
+
+                    if self.__semester_and_course == {}:
+                        self.__semester_and_course[nr] = {c: 1}
+                    elif nr not in self.__semester_and_course:
+                        self.__semester_and_course[nr] = {c: 1}
+
+                    elif nr in self.__semester_and_course:
+                        if c in self.__semester_and_course[nr]:
+                            count = self.__semester_and_course[nr][c] + 1
+                            self.__semester_and_course[nr][c] = count
+                        else:
+                            self.__semester_and_course[nr].update({c: 1})
+                    else:
+                        pass  # do nothing
+
+        print(self.__semester_and_course)
+
+        for semester, value in self.__semester_and_course.items():
+
+            sPath = "output/semesters/{}.html".format(semester)
+            with open(sPath, "w") as f:
+                f.write(HTML_FRAME_TOP.replace("{title}", "Semester " + semester).replace("{css_path}", "../"))
+                f.write("<h1>Semester {}</h1><table>".format(semester))
+                f.write("<thead>\n<tr><th>Course code</th><th>Name</th><th>#Students</th></tr>\n</thead>\n<tbody>")
+                for i, j in value.items():
+                    f.write("<tr><td>{}</td><td>{}</td><td>{}</td></tr>".format(i, self.__courses[i], j))
+                f.write("</tbody>\n</table>\n")
+                f.write(HTML_FRAME_BOTTOM)
 
     def __generate_index_file(self):
         """Generates the index HTML file."""
@@ -116,8 +181,20 @@ class Gradebook(object):
             f.write("</tbody>\n</table>\n")
 
             # list of courses
+            f.write("<h2>Courses</h2><table>\n<thead>\n<tr><th>Course code</th><th>Name</th></tr>\n</thead>\n<tbody>\n")
+            for course, name in sorted(self.__courses.items()):
+                f.write("<tr><td><a href=\"courses/{}.html\">{}</a></td><td>{}</td></tr>\n".format(course, course, name))
+            f.write("</tbody>\n</table>\n")
 
             # list of semesters
+            f.write("<h2>Semesters</h2><table>\n<thead>\n")
+            f.write("<tr><th>Semester</th><th>Courses</th></tr>\n</thead>\n<tbody>\n")
+            for semester, value in sorted(self.__semester_and_course.items()):
+                f.write("<tr><td><a href=\"semesters/{}.html\">{}</a></td><td>\n".format(semester, semester))
+                for i, j in sorted(value.items()):
+                    f.write("<a href=\"courses/{}.html\">{}</a> ".format(i, i))
+                f.write("</td></tr>\n")
+            f.write("</tbody>\n</table>\n")
 
             f.write(HTML_FRAME_BOTTOM)
 
@@ -125,9 +202,9 @@ class Gradebook(object):
         self.__create_folders()
         self.__load_data()
         self.__generate_student_files()
-        # self.__generate_course_files()
-        # self.__generate_semester_files()
-        # self.__generate_index_file()
+        self.__generate_course_files()
+        self.__generate_semester_files()
+        self.__generate_index_file()
 
 
 def main():
